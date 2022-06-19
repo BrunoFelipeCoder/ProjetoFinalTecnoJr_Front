db5d6aeaaf05d34434ad0c370ee64e17ad456d03
BrunoFelipeCoder <BrunoFelipeWork@gmail.com>
2022-06-19 01:49:32 -0300

    criando o js da pagina para confirmar o email

---
+++ js/confirmarEmail.js
@@ -0,0 +1,44 @@
+const email = document.getElementById("email");
+const codigo = document.getElementById("codigo");
+function mandaremail(event) {
+  event.preventDefault();
+  const url = "http://localhost:3000/auth/confirmar_email";
+  const mail = {
+    email: email.value,
+  };
+  console.log(mail);
+  let request = new XMLHttpRequest();
+  request.open("POST", url, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(JSON.stringify(mail));
+  request.onload = function () {
+    console.log(this.response);
+  };
+  console.log(this.response);
+  return request.response;
+}
+
+function verificarcodigo(event) {
+  event.preventDefault();
+  const url = "http://localhost:3000/auth/confirmar_codigo";
+  const mail = {
+    email: email.value,
+    codigo: codigo.value,
+  };
+  console.log(mail);
+  let request = new XMLHttpRequest();
+  request.open("POST", url, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(JSON.stringify(mail));
+  request.onload = function () {
+    if (this.response == "oi") {
+      console.log(this.response);
+      return window.location.replace(
+        "http://127.0.0.1:5500/html/cadastro.html"
+      );
+    } else {
+      console.log(this.response);
+      return alert("Codigo errado!");
+    }
+  };
+}
