ea7d65fea996618e2bb56a11ce44d17efe73323b
BrunoFelipeCoder <BrunoFelipeWork@gmail.com>
2022-06-18 21:16:56 -0300

    Adicionando o post para o BD e as verificações

--- js/cadastro.js
+++ js/cadastro.js
@@ -6,17 +6,56 @@ const nome_usuario = document.getElementById("nome_usuario");
 const senha = document.getElementById("senha");
 const confirmar_senha = document.getElementById("confirmar_senha");
 
-function mostrar(event) {
-  const formData = new FormData(form);
-  const formProps = Object.fromEntries(formData);
-  console.log(formProps);
-  console.log(
-    nome.value,
-    dataNasc.value,
-    email.value,
-    nome_usuario.value,
-    senha.value,
-    confirmar_senha.value
-  );
-  event.preventDefault();
+function cadastrarUsuario(url, user) {
+  let request = new XMLHttpRequest();
+  request.open("POST", url, true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(JSON.stringify(user));
+  request.onload = function () {
+    console.log(this.response);
+  };
+  console.log(this.response);
+  return request.response;
+}
+
+function conferirSenha(user) {
+  if (user.senha !== confirmar_senha.value) {
+    alert("As senhas não conferem!");
+    return false;
+  }
+  return true;
+}
+
+function conferirData(user) {
+  const data = new Date();
+  const [ano, mes, dia] = dataNasc.value.split("-").map(Number);
+  if (ano < 1900) {
+    alert("Ano inválido");
+    return false;
+  }
+  if (data.getFullYear() - ano < 13) {
+    alert("Idade mínima não atingida!");
+    return false;
+  }
+  return true;
+}
+
+function cadastro(event) {
+  const user = {
+    nome: nome.value,
+    email: email.value,
+    nomeUsuario: nome_usuario.value,
+    senha: senha.value,
+  };
+  if (!conferirSenha(user)) {
+    event.preventDefault();
+    return false;
+  }
+  if (!conferirData(user)) {
+    event.preventDefault();
+    return false;
+  }
+  const url = "http://localhost:3000/auth/cadastro";
+  cadastrarUsuario(url, user);
+  return alert("Usuário Cadastrado com sucesso!");
 }
