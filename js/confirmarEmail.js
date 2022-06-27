ab2dfe7994e368ffb1d8a1c70666616c52cc8728
BrunoFelipeCoder <BrunoFelipeWork@gmail.com>
2022-06-26 21:47:54 -0300

    Mandando link de cadastro e não código

--- js/confirmarEmail.js
+++ js/confirmarEmail.js
@@ -1,45 +1,19 @@
 const email = document.getElementById("email");
-const codigo = document.getElementById("codigo");
-function mandaremail(event) {
-  event.preventDefault();
-  //const url = "http://localhost:3000/auth/confirmar_email";
-  const url = "https://ola-dev-backend.herokuapp.com/auth/confirmar_email";
-  const mail = {
-    email: email.value,
-  };
-  console.log(mail);
-  let request = new XMLHttpRequest();
-  request.open("POST", url, true);
-  request.setRequestHeader("Content-Type", "application/json");
-  request.send(JSON.stringify(mail));
-  request.onload = function () {
-    console.log(this.response);
-  };
-  console.log(this.response);
-  return request.response;
-}
 
-function verificarcodigo(event) {
+function mandarEmail(event) {
   event.preventDefault();
-  //const url = "https://ola-dev-backend.herokuapp.com/auth/confirmar_codigo";
+  const url = "http://localhost:3000/auth/confirmar_email";
+  //const url = "https://ola-dev-backend.herokuapp.com/auth/confirmar_email";
   const mail = {
     email: email.value,
-    codigo: codigo.value,
   };
-  console.log(mail);
   let request = new XMLHttpRequest();
   request.open("POST", url, true);
   request.setRequestHeader("Content-Type", "application/json");
   request.send(JSON.stringify(mail));
-  request.onload = function () {
-    if (this.response == "oi") {
-      console.log(this.response);
-      return window.location.replace(
-        "http://127.0.0.1:5500/html/cadastro.html"
-      );
-    } else {
-      console.log(this.response);
-      return alert("Codigo errado!");
-    }
+  request.onload = async function () {
+    console.log(this.response);
+    alert(this.response);
   };
+  return request.response;
 }
