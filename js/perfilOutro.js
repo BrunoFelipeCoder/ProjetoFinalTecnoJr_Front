0dd69e669d39dd1f9a1b673aacf1db06603f7572
BrunoFelipeCoder <BrunoFelipeWork@gmail.com>
2022-07-31 15:50:04 -0300

    add mudança de tema do outro usuario

--- js/perfilOutro.js
+++ js/perfilOutro.js
@@ -7,48 +7,51 @@ request.open("POST", url, true);
 request.setRequestHeader("Content-Type", "application/json");
 request.send(JSON.stringify(myParam));
 request.onload = () => {
-  user = JSON.parse(request.response);
-  document.querySelector("#nome").innerText = user.nome;
-  document.querySelector("#usuario").innerText = user.nomeUsuario;
-  document.querySelector("#sobre").innerText = user.sobreMim;
-  document.querySelector("#header").src = user.imgBanner;
-  document.querySelector("#foto").src = user.imgPerfil;
+	user = JSON.parse(request.response);
+	document.querySelector("#nome").innerText = user.nome;
+	document.querySelector("#usuario").innerText = user.nomeUsuario;
+	document.querySelector("#sobre").innerText = user.sobreMim;
+	document.querySelector("#header").src = user.imgBanner;
+	document.querySelector("#foto").src = user.imgPerfil;
+	document
+		.querySelector("body")
+		.setAttribute("style", "--themecolor: #" + user.corTema);
 };
 
 function buscar(e) {
-  const resultados = document.getElementById("resultados");
-  let match = e.value.match(/^[a-zA-Z]*/);
-  let match2 = e.value.match(/\s*/);
-  if (match2[0] === e.value) {
-    resultados.innerHTML = "";
-    return;
-  }
-  if (match[0] === e.value) {
-    url = "http://localhost:3000/request/buscar";
-    //url = "https://ola-dev-backend.herokuapp.com/request/buscar";
-    let request = new XMLHttpRequest();
-    request.open("POST", url, true);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send(
-      JSON.stringify({
-        userName: e.value,
-      })
-    );
-    request.onload = () => {
-      let usuario = JSON.parse(request.response);
-      resultados.innerHTML = "";
-      if (usuario.usuario.length < 1) {
-        resultados.innerHTML = `<p>Não foram encontrados resultados!</p>`;
-        return;
-      }
-      usuario.usuario.forEach((user, index) => {
-        if (index > 0) resultados.innerHTML += `<hr>`;
-        if (user.chave == localStorage.getItem("chave"))
-          resultados.innerHTML += `<a href="./perfil.html">${user.nomeUsuario}</a>`;
-        else
-          resultados.innerHTML += `<a href="./perfilOutro.html?userID=${user.nomeUsuario}">${user.nomeUsuario}</a>`;
-      });
-    };
-    return;
-  }
+	const resultados = document.getElementById("resultados");
+	let match = e.value.match(/^[a-zA-Z]*/);
+	let match2 = e.value.match(/\s*/);
+	if (match2[0] === e.value) {
+		resultados.innerHTML = "";
+		return;
+	}
+	if (match[0] === e.value) {
+		url = "http://localhost:3000/request/buscar";
+		//url = "https://ola-dev-backend.herokuapp.com/request/buscar";
+		let request = new XMLHttpRequest();
+		request.open("POST", url, true);
+		request.setRequestHeader("Content-Type", "application/json");
+		request.send(
+			JSON.stringify({
+				userName: e.value,
+			})
+		);
+		request.onload = () => {
+			let usuario = JSON.parse(request.response);
+			resultados.innerHTML = "";
+			if (usuario.usuario.length < 1) {
+				resultados.innerHTML = `<p>Não foram encontrados resultados!</p>`;
+				return;
+			}
+			usuario.usuario.forEach((user, index) => {
+				if (index > 0) resultados.innerHTML += `<hr>`;
+				if (user.chave == localStorage.getItem("chave"))
+					resultados.innerHTML += `<a href="./perfil.html">${user.nomeUsuario}</a>`;
+				else
+					resultados.innerHTML += `<a href="./perfilOutro.html?userID=${user.nomeUsuario}">${user.nomeUsuario}</a>`;
+			});
+		};
+		return;
+	}
 }
